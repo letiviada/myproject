ad952b8ef90a5180a2758e075fba9419c75e07e9
letiviada <99149684+letiviada@users.noreply.github.com>
2023-02-01 10:22:20 +0000

    Add files via upload

---
+++ Roll a dice.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+import random
+
+def roll():
+    x=random.randint(1,6)
+    lbl["text"]=f'''{x}'''
+
+window=tk.Tk()
+window.title("Roll a six-sided die")
+window.columnconfigure(0,minsize=250,weight=1)
+window.rowconfigure([0,1],minsize=50,weight=1)
+
+btn=tk.Button(master=window, text="Roll",command=roll)
+btn.grid(row=0,column=0,sticky="nsew")
+
+lbl=tk.Label(master=window, text="1")
+lbl.grid(row=1,column=0,sticky="nsew")
+
+window.mainloop()
